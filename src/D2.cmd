5d2b3b677f22970cb3ef2b2c251577dbd8cf965f
Seraphina Goldfarb-Tarrant <serif@dryas.ling.washington.edu>
2018-04-23 22:23:22 -0700

    final version of D2.cmd

---
+++ src/D2.cmd
@@ -0,0 +1,10 @@
+universe=vanilla
+executable=run_summarization.sh
+getenv=true
+output=baseline.out
+error=baseline.err
+log=baseline.log
+arguments="lexrank ../conf/patas_devtest_config.yaml /dropbox/17-18/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml ../outputs/D2/ /dropbox/17-18/573/Data/models/devtest/"
+notification=complete
+transfer_executable=false
+queue
