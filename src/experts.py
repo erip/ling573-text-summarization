db08e793213fa1608232bf51330c3101da88ca5e
Elijah Rippeth <elijah.rippeth@gmail.com>
2018-05-10 20:12:29 -0400

    add chronological expert.

--- src/experts.py
+++ src/experts.py
@@ -12,3 +12,25 @@ class Expert(ABC):
     @abstractmethod
     def order(self, d1, d2, partial_summary):
         """Orders two documents."""
+
+class ChronologicalExpert(Expert):
+    def __init__(self):
+        self._name = "chronological"
+
+    def order(self, d1, d2, partial_summary):
+        sents_from_same_doc = d1.doc_id() == d2.doc_id()
+        both_docs_have_time_stamp = d1.get_timestamp() and d2.get_timestamp()
+        doc1_sent_index = d1.get_sent_index()
+        doc2_sent_index = d2.get_sent_index()
+
+        # T(u) < T(v)
+        if both_docs_have_time_stamp and d1.get_timestamp() < d2.get_timestamp():
+            return 1
+        # [D(u) == D(v)] & [N(u) < N(v)]
+        elif sents_from_same_doc and doc1_sent_index < doc2_sent_index:
+            return 1
+        # [T(u) == T(v)] & [D(u) != D(v)]
+        elif d1.get_timestamp() == d2.get_timestamp() and not sents_from_same_doc:
+            return 0.5
+        # otherwise
+        return 0
--- tests/doc_utils_test.py
+++ tests/doc_utils_test.py
@@ -24,10 +24,10 @@ class DocumentTestCase(TestCase):
 
         self.assertTrue(doc.is_aquaint2)
         self.assertEqual(doc.yyyy, year)
-        self.assertEquals(doc.mm, month)
-        self.assertEquals(doc.dd, day)
-        self.assertEquals(doc.lang, lang)
-        self.assertEquals(doc.src, src)
+        self.assertEqual(doc.mm, month)
+        self.assertEqual(doc.dd, day)
+        self.assertEqual(doc.lang, lang)
+        self.assertEqual(doc.src, src)
 
     @skipUnless(os.path.sep == "/", "Test only works for unix systems")
     def test_aquaint2_to_path(self):
@@ -42,8 +42,8 @@ class DocumentTestCase(TestCase):
         doc = Document(DocumentTestCase.base_paths, doc_id)
 
         expected_path = "/aquaint2/data/{0}_{1}/{0}_{1}_{2}{3}.xml".format(src, lang, year, month).lower()
-        self.assertEquals(doc.get_path(), expected_path)
-        self.assertEquals(doc.id(), doc_id)
+        self.assertEqual(doc.get_path(), expected_path)
+        self.assertEqual(doc.id(), doc_id)
 
     def test_aquaint_parse(self):
         year, month, day = ("1999", "03", "30")
@@ -58,9 +58,9 @@ class DocumentTestCase(TestCase):
 
         self.assertTrue(doc.is_aquaint)
         self.assertEqual(doc.yyyy, year)
-        self.assertEquals(doc.mm, month)
-        self.assertEquals(doc.dd, day)
-        self.assertEquals(doc.src, src)
+        self.assertEqual(doc.mm, month)
+        self.assertEqual(doc.dd, day)
+        self.assertEqual(doc.src, src)
 
     @skipUnless(os.path.sep == "/", "Test only works for unix systems")
     def test_aquaint_xie_to_path(self):
@@ -75,8 +75,8 @@ class DocumentTestCase(TestCase):
         doc = Document(DocumentTestCase.base_paths, doc_id)
 
         expected_path = "/aquaint/{0}/{1}/{1}{2}{3}_{4}_ENG".format(src.lower(), year, month, day, real_src)
-        self.assertEquals(doc.get_path(), expected_path)
-        self.assertEquals(doc.id(), doc_id)
+        self.assertEqual(doc.get_path(), expected_path)
+        self.assertEqual(doc.id(), doc_id)
 
 
     @skipUnless(os.path.sep == "/", "Test only works for unix systems")
@@ -91,8 +91,8 @@ class DocumentTestCase(TestCase):
         doc = Document(DocumentTestCase.base_paths, doc_id)
 
         expected_path = "/aquaint/{0}/{1}/{1}{2}{3}_{4}_ENG".format(src.lower(), year, month, day, src)
-        self.assertEquals(doc.get_path(), expected_path)
-        self.assertEquals(doc.id(), doc_id)
+        self.assertEqual(doc.get_path(), expected_path)
+        self.assertEqual(doc.id(), doc_id)
 
     @skipUnless(os.path.sep == "/", "Test only works for unix systems")
     def test_aquaint_apw_to_path(self):
@@ -106,5 +106,5 @@ class DocumentTestCase(TestCase):
         doc = Document(DocumentTestCase.base_paths, doc_id)
 
         expected_path = "/aquaint/{0}/{1}/{1}{2}{3}_{4}_ENG".format(src.lower(), year, month, day, src)
-        self.assertEquals(doc.get_path(), expected_path)
-        self.assertEquals(doc.id(), doc_id)
+        self.assertEqual(doc.get_path(), expected_path)
+        self.assertEqual(doc.id(), doc_id)
