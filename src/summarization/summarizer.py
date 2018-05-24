2935bee3ab4e13df26d094d16fcb5ce2a70b8edb
Elijah Rippeth <elijah.rippeth@gmail.com>
2018-05-24 16:00:42 -0400

    add main.

--- src/summarization/summarizer.py
+++ src/summarization/summarizer.py
@@ -64,3 +64,5 @@ if __name__ == "__main__":
     }
 
     summarizer = Summarizer.from_config(config, nlp)
+
+
--- src/summarize.py
+++ src/summarize.py
@@ -4,6 +4,7 @@ import spacy
 
 from summarization.strategy import SummarizationStrategy, LexRankSummarizationStrategy
 from summarization.utils import Embedder, SpacyEmbedder, TfidfEmbedder
+from summarization.information_ordering import InformationOrderer, ChronologicalExpert
 from summarization import Summarizer
 
 from corpus import Corpus
@@ -16,15 +17,24 @@ def setup_argparse():
                    help='a yaml config mapping the topic clustering to file locations')
     p.add_argument('-t', dest='topic_file', default='../conf/GuidedSumm_MINE_test_topics.xml',
                    help='an AQUAINT config file with topic clustering')
-    p.add_argument('-n', dest='num_words', help='maximum number of words allowed in summary', type=int, default=100)
-    p.add_argument('-v', dest='vector_type', help='the type of vector representation to use for sentences. Choose from:'
-                                                  'spacy, doc2vec, tfidf, word2vec',  default='tfidf')
-    p.add_argument('-th', dest='threshold', default=0.1, type=float, help='threshold for when to draw a edge between sentences in lexrank')
-    p.add_argument('-e', dest='epsilon', default=0.1, type=float, help='epsilon value to control convergence of eigenvectors in lexrank matrix')
-    p.add_argument('-d', dest='output_dir', default='../outputs/D2/', help='dir to write output summaries to')
+    p.add_argument('-d', dest='output_dir', default='../outputs/D4/', help='dir to write output summaries to')
     p.add_argument('-m', dest='model_path', default='', help='path for a pre-trained embedding model')
     return p.parse_args()
 
+def make_filename(topic_id, num_words):
+    """Given topic id and max num words, return filename for storing summary
+    :param topic_id: A string representing the unique topic id
+    :param num_words: A maximum number of words for the summary length"""
+    some_unique_alphanum = 4  # this is our groupnumber for identification
+    return '{0}-A.M.{2}.{1}.{3}'.format(topic_id[:-1], topic_id[-1], num_words, some_unique_alphanum)
+
+def setup_information_orderer():
+
+    chronological_expert = ChronologicalExpert()
+    experts = { chronological_expert }
+    weights = { ChronologicalExpert.name: 1.0 }
+
+    return InformationOrderer(experts, weights)
 
 if __name__ == "__main__":
 
@@ -49,8 +59,14 @@ if __name__ == "__main__":
 
     summarizer = Summarizer.from_config(config, nlp)
 
+    information_orderer = setup_information_orderer()
+
     for topic in corpus.topics:
         candidates = summarizer.summarize(topic)
-        print("Candidates:")
-        for can in candidates:
-            print(can._sent)
+        print(len(candidates))
+        summary = information_orderer.order_all(candidates)
+        print(len(summary))
+        with open('{0}{1}'.format(args.output_dir, make_filename(topic.id(), config.get(Summarizer.WORD_LIMIT_KEY))), 'w') as outfile:
+            for sentence in summary:
+                outfile.write('{}\n'.format(sentence.text))
+            outfile.write('\n')  # write blank file if no candidates
