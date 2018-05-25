090852e7deaab522bae78b272a6ed62b38fb5a0e
Elijah Rippeth <elijah.rippeth@gmail.com>
2018-05-25 16:22:00 -0400

    remove unnecessary tests.

--- tests/example_expert.py
+++
@@ -1,9 +0,0 @@
-# from src.summarization.information_ordering.expert import Expert
-#
-# class ExampleExpert(Expert):
-#     def __init__(self):
-#         self._name = "example"
-#
-#     def order(self, d1, d2, partial_summary):
-#         """This expert will always place d1 ahead of d2"""
-#         return 0.51
