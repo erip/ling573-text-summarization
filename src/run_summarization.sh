30853f455976372d4b4c123c3caa2b8824f86458
seraphinatarrant <serif@uw.edu>
2018-05-25 17:42:07 -0700

    fixes to run_summarization.sh to reflect getting rid for the  command line arg, and to put back in support for adding a model file

--- src/run_summarization.sh
+++ src/run_summarization.sh
@@ -3,12 +3,12 @@
 # example run command:
 #first sentences
 #training
-# ./run_summarization.sh ../conf/patas_config.yaml ../conf/UpdateSumm09_test_topics.xml ../outputs/D#/ /dropbox/17-18/573/Data/models/training/2009/ ../results/D3_rouge_scores.out some_experiment_name
+# ./run_summarization.sh ../conf/patas_config.yaml ../outputs/D#/ /dropbox/17-18/573/Data/models/training/2009/ ../results/D3_rouge_scores.out some_experiment_name
 #devtest
-# ./run_summarization.sh ../conf/patas_devtest_config.yaml /dropbox/17-18/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml ../outputs/D#/ /dropbox/17-18/573/Data/models/devtest/ ../results/D3_rouge_scores.out some_experiment_name
+# ./run_summarization.sh ../conf/patas_devtest_config.yaml ../outputs/D#/ /dropbox/17-18/573/Data/models/devtest/ ../results/D3_rouge_scores.out some_experiment_name
 
 #example run command with pretrained model
-#TODO add this
+# ./run_summarization.sh ../conf/patas_devtest_config.yaml ../outputs/D#/ /dropbox/17-18/573/Data/models/devtest/ ../results/D3_rouge_scores.out some_experiment_name
 
 config_file=$1
 output_dir=$2
@@ -19,7 +19,12 @@ vec_model_path=${6:-""}
 rouge_dir="/mnt/dropbox/17-18/573/code/ROUGE/"
 rouge_config_file="../results/rouge_config_${experiment_number}.xml"
 
-python3 summarize.py -c ${config_file} -t ${topic_cluster_file} -d ${output_dir} 
+if [ vec_model_path = "" ]
+then
+    python3 summarize.py -c ${config_file} -d ${output_dir}
+else
+    python3 summarize.py -c ${config_file} -d ${output_dir} -m ${vec_model_path}
+fi
 
 # generate ROUGE config file
 python3 "${rouge_dir}/create_config.py" ${output_dir} ${model_dir} ${rouge_config_file}
