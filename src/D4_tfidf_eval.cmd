95257aa2781719725dc72a8e893ebe98a2f14261
Seraphina Goldfarb-Tarrant <serif@dryas.ling.washington.edu>
2018-05-30 14:57:22 -0700

    minor change to make dir structure naming consistent

--- src/D4_tfidf_eval.cmd
+++ src/D4_tfidf_eval.cmd
@@ -4,7 +4,7 @@ getenv=true
 output=d4_tfidf_eval.out
 error=d4_tfidf_eval.err
 log=d4_tfidf_eval.log
-arguments="../conf/patas_eval_tfidf_config.yaml ../outputs/d4/tfidf/ /dropbox/17-18/573/Data/models/evaltest/ ../results/d4_rouge_scores_tfidf.out eval"
+arguments="../conf/patas_eval_tfidf_config.yaml ../outputs/D4/tfidf/ /dropbox/17-18/573/Data/models/evaltest/ ../results/D4_rouge_scores_tfidf.out eval"
 notification=complete
 transfer_executable=false
 queue
