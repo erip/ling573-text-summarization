923676b023282f452fc8fe73a3ccf27b08b0721a
Elijah Rippeth <elijah.rippeth@gmail.com>
2018-05-30 18:45:03 -0700

    run devtest and evaltest with redundancy threshold of 0.6

--- src/D4_tfidf.cmd
+++ src/D4_tfidf.cmd
@@ -1,10 +1,10 @@
 universe=vanilla
 executable=run_summarization.sh
 getenv=true
-output=d4_tfidf.out
-error=d4_tfidf.err
-log=d4_tfidf.log
-arguments="../conf/patas_devtest_tfidf_config.yaml ../outputs/d4/tfidf/ /dropbox/17-18/573/Data/models/devtest/ ../results/d4_rouge_scores_tfidf.out experiment_two"
+output=D4_tfidf.out
+error=D4_tfidf.err
+log=D4_tfidf.log
+arguments="../conf/patas_devtest_tfidf_config.yaml ../outputs/D4_devtest/ /dropbox/17-18/573/Data/models/devtest/ ../results/D4_devtest_rouge_scores.out devtest"
 notification=complete
 transfer_executable=false
 queue
